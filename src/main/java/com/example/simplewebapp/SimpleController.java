157cffbc018b94a88350e3900d9fa7b3c55b1fce
Maxim Uvarenkov <maxim.uvarenkov@gs-labs.ru>
2021-06-10 15:32:16 +0300

    Initial

---
+++ src/main/java/com/example/simplewebapp/SimpleController.java
@@ -0,0 +1,15 @@
+package com.example.simplewebapp;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController
+{
+    @RequestMapping("/")
+    public String index()
+    {
+        return "Hello, Java World!\n";
+    }
+}
---
+++ src/main/java/com/example/simplewebapp/SimpleWebAppApplication.java
@@ -0,0 +1,40 @@
+package com.example.simplewebapp;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.CommandLinePropertySource;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class SimpleWebAppApplication
+{
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleWebAppApplication.class, args);
+    }
+
+
+    @Bean
+    public CommandLineRunner runner(ApplicationContext ctxt)
+    {
+        return args ->
+        {
+            System.out.println("Trolllololo!");
+
+            String[] beanNames = ctxt.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+
+            for (String name : beanNames)
+            {
+                System.out.println(name);
+            }
+
+        };
+    }
+
+
+}
